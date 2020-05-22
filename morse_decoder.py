ff86022adde1a014360c51c9dc44a338b43ec05d
Mikhael <Mikhael1729@gmail.com>
2020-05-22 01:04:12 -0400

    Solve the problem in team

---
+++ morse_decoder.py
@@ -0,0 +1,60 @@
+letter_to_morse = {
+  '.-': 'A',
+  '-..': 'B',
+  '-.-.': 'C',
+  '-..': 'D',
+  '.': 'E',
+  '..-.': 'F',
+  '--.': 'G',
+  '....': 'H',
+  '..': 'I',
+  '.---': 'J',
+  '-.-': 'K',
+  '.-..': 'L',
+  '--': 'M',
+  '-.': 'N',
+  '---': 'O',
+  '.--.': 'P',
+  '--.-': 'Q',
+  '.-.': 'R',
+  '...': 'S',
+  '-': 'T',
+  '..-': 'U',
+  '...-': 'V',
+  '.--': 'W',
+  '-..-': 'X',
+  '-.--': 'Y',
+  '--..': 'Z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
+}
+converted_phrase = ''
+morse_code = input("Escriba el código morse: ")
+
+# Separate letters by |
+words = morse_code.split()
+
+for word in words:
+  converted_word = ''
+  letters = word.split('|')
+
+  for letter in letters:
+    converted_word = converted_word + letter_to_morse[letter]
+
+  converted_phrase = converted_phrase + converted_word + ' '
+
+print(f'converted_prhase: {converted_phrase}')
+# LOL claro! SPLit es solo para strings. X"D
+
+# for part in letters:
+#   print(part.split())
+
+# print(letters)
